82da24d7121557ba94cb2b76793fa4f691e70568
ripon4521 <riponakondo4521@gmail.com>
2026-01-31 14:45:47 +0600

    practise loops

---
+++ visual.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+    int num1;
+    int num2;
+    int sum;
+    cout << "Enter first number: " << endl;
+    cin >> num1;
+    cout << "Enter second number: " <<endl;
+    cin >> num2;
+    sum = num1 + num2;
+    cout << "The sum is: " << sum << endl;
+    return 0;
+}
